ba12320ca0c02e36156be90148c3a1f4435a6e5b
leeehh <527317186@qq.com>
2026-09-22 05:42:32 +0000

    fix sbi asm x16

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -10,11 +10,11 @@ fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let mut ret;
     unsafe {
         asm!(
-            "li x16, 0",
             "ecall",
             inlateout("x10") arg0 => ret,
             in("x11") arg1,
             in("x12") arg2,
+            in("x16") 0,
             in("x17") which,
         );
     }
